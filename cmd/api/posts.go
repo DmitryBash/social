dfef8be3e430423de1acbeff59942ac65df54236
Batracov Dmitrii <db@Batracovs-MacBook-Pro.local>
2025-03-10 09:57:01 +0100

    add show action for posts

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -3,6 +3,11 @@ package main
 import (
 	"net/http"
 	"social/internal/store"
+	"strconv"
+
+	"errors"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
@@ -31,6 +36,32 @@ func (app *application) createPostsHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx := r.Context()
+	post, err := app.store.Posts.GetByID(ctx, id)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
